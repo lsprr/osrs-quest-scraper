a6fb98da423a1561a5b2ec76bf7269235bffba53
lsprr <16653744+lsprr@users.noreply.github.com>
2023-05-24 12:33:44 -0400

    Improve express server with logging and error handling

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 import express from 'express';
 import fs from 'fs/promises';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
+import morgan from 'morgan';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/api/quests', async (req, res) => {
+app.use(morgan('combined'));
+
+app.get('/api/quests', async (req, res, next) => {
     try {
         const data = await fs.readFile(path.join(__dirname, 'data.json'));
         const quests = JSON.parse(data);
         res.json(quests);
     } catch (err) {
-        res.status(500).send({ message: 'Error reading quests data.' });
+        next(err);
     }
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send({message: 'Internal server error.'});
 });
 
 app.listen(port, () => {
